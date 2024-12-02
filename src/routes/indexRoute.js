65b892da073166dad2c65c0b43b36802c39e9c10
root <root@noname.localdomain>
2024-12-03 00:44:45 +0100

    ...

--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -8,14 +8,14 @@ const router = express.Router();
 router.use(express.json())
 
 
-router.use((req, res, next) => {
-  console.log("++++++++++++++++++ indexRouter +++++++++++++++++++++")
-  if (req.path.startsWith("/adm/services/sign") || req.method === 'GET') {
-    return next();
-  } 
-  checkJwt(req, res, next);
-  //next()
-});
+// router.use((req, res, next) => {
+//   console.log("++++++++++++++++++ indexRouter +++++++++++++++++++++")
+//   if (req.path.startsWith("/adm/services/sign") || req.method === 'GET') {
+//     return next();
+//   } 
+//   checkJwt(req, res, next);
+//   //next()
+// });
 
 router.use('/public/adm', checkPermissions(), fileRouter)   
 router.use('/public/cmn', checkPermissions(), fileRouter)   
